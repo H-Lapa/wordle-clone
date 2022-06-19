8c45e91b962df097fecb99960e892bbbf9ca09cd
Hugo Lapa <Hugolapa888@gmail.com>
2022-06-19 21:28:08 +0100

    basic letter made

---
+++ components/Keyboard.js
@@ -0,0 +1,15 @@
+
+const Keyboard = () => {
+
+    return (
+        //keyboard
+        <div className="max-w-full">
+            {/* letter */}
+            <div className="rounded-lg bg-gray-500 p-3 w-fit m-5">
+                <p className="">A</p>
+            </div>
+        </div>
+    )
+}
+
+export default Keyboard;
