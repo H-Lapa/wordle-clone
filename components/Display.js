4b831fc43c67c8f830fd12d3336fd315bdbcfcd6
Hugo Lapa <Hugolapa888@gmail.com>
2022-06-21 20:43:06 +0100

    Display for words

---
+++ components/Display.js
@@ -0,0 +1,21 @@
+
+const Display = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="grid grid-cols-5 gap-4 m-5">
+
+                <div className="bg-gray-500 p-5 rounded text-center">1</div>
+                <div className="bg-gray-500 p-5 rounded text-center">2</div>
+                <div className="bg-gray-500 p-5 rounded text-center">3</div>
+                <div className="bg-gray-500 p-5 rounded text-center">4</div>
+                <div className="bg-gray-500 p-5 rounded text-center">5</div>
+
+
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default Display;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Keyboard from '../components/Keyboard'
+import Display from '../components/Display'
 
 export default function Home() {
   return (
     <div className=''>
+      <div>
+        <Display></Display>
+      </div>
+
       <div className='absolute bottom-0 w-11/12 m-auto flex'>
         <Keyboard/>
       </div>
